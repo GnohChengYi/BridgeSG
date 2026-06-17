b038d66f8b53a11fe2e5a3462781a431ac3ae3ad
GnohChengYi <chengyignoh@gmail.com>
2026-06-17 22:18:53 +0800

    allow callback & inline query updates

--- set_webhook.ps1
+++ set_webhook.ps1
@@ -1,11 +1,12 @@
-# Replace $Token with actual bot token
-# Copy entire code and paste in terminal
+# PowerShell -ExecutionPolicy Bypass -File .\set_webhook.ps1
 
-$Token = "<YOUR_BOT_TOKEN>"
+$Token = ""
 $WebhookUrl = "https://bridge-sg.vercel.app/telegram"
 
-# Construct the URI with properly escaped allowed_updates JSON array
-$Uri = "https://api.telegram.org/bot$Token/setWebhook?url=$WebhookUrl&allowed_updates=[`"message`",`"channel_post`"]"
+$AllowedUpdates = '["message","channel_post","callback_query","inline_query"]'
+
+$BaseUrl = "https://api.telegram.org/bot{0}/setWebhook" -f $Token
+$Uri = "{0}?url={1}&allowed_updates={2}" -f $BaseUrl, $WebhookUrl, $AllowedUpdates
 
 # Make the request to Telegram
 Invoke-RestMethod -Uri $Uri -Method Get
